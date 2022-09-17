bff6ed3708a207ec11c258a995b2f584258457b9
Viswesh Suresh <visweshsuresh@gmail.com>
2022-09-17 21:53:54 +0530

    Added comment

--- Week 1/PPA1.py
+++ Week 1/PPA1.py
@@ -1,3 +1,7 @@
+'''Twin primes are pairs of prime numbers that differ by 2. For example (3, 5), (5, 7), and (11,13) are twin primes.
+Write a function Twin_Primes(n, m) where n and m are positive integers and n < m , that returns all unique twin primes between m and n (both inclusive). The function returns a list of tuples and each tuple (a,b) represents one unique twin prime where n <= a < b <= m.'''
+
+
 def Twin_Primes(n,m):
     fin=[]
     def isprime(x):
