8c671090ccf9baceff1b2c197f3170b2c4137082
Viswesh Suresh <visweshsuresh@gmail.com>
2022-09-19 07:42:57 +0530

    Corrected error

--- Week 1/GRPA1_Ver2.py
+++ Week 1/GRPA1_Ver2.py
@@ -11,7 +11,8 @@ def find_Min_Difference(L, P):
     L.sort()
     diff = maxsize
     for i in range(len(L) - P + 1):
-        diff = min(diff, L[i + P - 1]-L[i])
+        if L[i + P - 1]-L[i]<diff:
+            diff = L[i + P - 1]-L[i]
     return diff 
 L=eval(input().strip())
 P=int(input())
